875cfadccf30850e29aa79db4c4e9f4ca0cbbab1
LuisMarques666 <luishenriquemarques666@gmail.com>
2024-08-31 11:52:19 -0300

    Update exercise02.js

--- exercise02.js
+++ exercise02.js
@@ -1,7 +1,6 @@
 // Exercise 2
-// Crie um programa que classifica a idade de uma pessoa em categorias (criança,
-// adolescente, adulto, idoso) com base no valor fornecido, utilizando uma estrutura de
-// controle if-else.
+// Um programa que classifica a idade de uma pessoa em categorias (criança, adolescente, adulto, idoso) 
+// com base no valor fornecido, utilizando uma estrutura decontrole if-else.
 
 const prompt = require('prompt-sync')()
 
