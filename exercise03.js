02881f3478e1026f08fb2cc6fb113a1debc9acab
LuisMarques666 <luishenriquemarques666@gmail.com>
2024-08-31 11:52:41 -0300

    Update exercise03.js

--- exercise03.js
+++ exercise03.js
@@ -1,6 +1,6 @@
 // Exercise 3
-// Implemente um programa que recebe uma nota de 0 a 10 e classifica como
-// "Aprovado", "Recuperação", ou "Reprovado" utilizando if-else if.
+// O programa recebe uma nota de 0 a 10 e classifica como"Aprovado", "Recuperação"
+// ou "Reprovado" utilizando if-else if.
 
 const prompt = require('prompt-sync')()
 
